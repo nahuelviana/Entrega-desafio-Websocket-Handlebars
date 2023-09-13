6b86b2b20a5ee6b3d278e8cd5fe2abff3ffa8889
Nahuel Rodriguez Viana <nahuelviana1@gmail.com>
2023-09-12 22:36:09 -0300

    repositorio creado

---
+++ ejemplos.js
@@ -0,0 +1,102 @@
+/* 
+if (nombre.includes(`rodri`)){
+    console.log(`existe`)
+}else {
+    console.log("no existe")
+} */
+
+
+
+/* let cadena = `        hola`
+console.log(cadena)
+console.log(cadena.trim())
+
+
+let mensaje = [];
+
+let intentoDeMesaje = `     hola    `;
+
+if (intentoDeMesaje.trim(). length>0 ){
+    mensaje.push(intentoDeMesaje.trim())
+}else{
+    console.log(`mensaje vacio`)
+}
+console.log(mensaje)
+
+
+let arrayAnidado = [1, 365, [1,2,3], "b", ["a", "b"]]
+console.log(arrayAnidado.flat())
+
+let variablePrueba = 0 ;
+
+let variable = variablePrueba || "sin valor"
+
+
+ */
+
+/* /* class Contador {
+
+    static cuentaGlobal = 0;
+
+    constructor (responsable){
+        this.responsable = responsable
+        this.cuentaIndividual = 0
+    }
+    getResponsable (){
+        return this.responsable
+    }
+    getCuentaIndividual(){
+        return this.getCuentaIndividual
+    }
+    getCuentaGlobal (){
+        return Contador.cuentaGlobal;
+    }
+    contar(){
+        this.cuentaIndividual++;
+        Contador.cuentaGlobal++;
+    }
+}
+ */
+
+
+/* const r1 = new Contador("contador1");
+const r2 = new Contador("contador2");
+r1.contar();
+r1.contar();
+r2.contar();
+console.log('r1 individual', r1.cuentaIndividual)
+console.log('r1 gobal: ', r1.getCuentaGlobal)
+console.log('r2 global: ', r2.getCuentaGlobal()) */ 
+
+/* 
+let numeros = [1,2,3,4,5,6]
+
+let totales = numeros.map ((numero, indice) => numero**indice)
+console.log(totales)
+
+let nombre = [`juan`, `pedro`, `maria`, `jose`, `grabriel`]
+nombre.includes (`pedro`)
+
+if (nombre.includes(`rodri`)){
+    console.log(`existe`)
+}else {
+    console.log("no existe")
+} */
+
+
+
+/* let impuestos = {
+    impuesto1: "2456",
+    impuesto2: "2454",
+    impuesto3 :"2457",
+    impuesto4 :"2458",
+    impuesto5 :"2459",
+}
+let parValor = Object.entries (impuestos);
+console.log(parValor)
+
+let soloPropiedades = Object.keys (impuestos)
+console.log(soloPropiedades)
+
+let soloValores = Object.values (impuestos)
+console.log(soloValores) */
---
+++ index.js
@@ -0,0 +1,55 @@
+class ProductManager {
+    constructor() {
+    this.products = [];
+    }
+
+    addProduct(product) {
+    if (this.products.some((p) => p.code === product.code)) {
+        throw new Error("El producto con el mismo código ya existe.");
+    }
+    this.products.push(product);
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProductById(id) {
+    const product = this.products.find((p) => p.id === id);
+    if (!product) {
+        throw new Error("Producto no encontrado.");
+    }
+    return product;
+    }
+}
+
+const productManager = new ProductManager();
+
+const newProduct = {
+    id: 1, 
+    title: "producto prueba",
+    descripcion: "este es un producto de prueba",
+    price: 200,
+    thumbnail: "sin imagen",
+    code: "abc123",
+    stock: 25,
+};
+productManager.addProduct(newProduct);
+
+
+console.log("Productos después de agregar:", productManager.getProducts());
+
+
+try {
+    productManager.addProduct(newProduct);
+} catch (error) {
+    console.error(error.message);
+}
+
+
+try {
+    const product = productManager.getProductById(1); 
+    console.log("Producto encontrado por ID:", product);
+} catch (error) {
+    console.error(error.message);
+}
