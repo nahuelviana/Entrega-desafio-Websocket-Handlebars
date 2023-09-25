3e231edd2e1eb1365e544086f0778c30adb27f46
Nahuel Rodriguez Viana <nahuelviana1@gmail.com>
2023-09-25 18:12:32 -0300

    listo para 3ra preentrega

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ class ProductManager {
     }
 
     getProductById(id) {
+        console.log("Buscando producto con ID:", id); // Agrega este mensaje de registro
         const product = this.products.find((p) => p.id === id);
         if (!product) {
             throw new Error("Producto no encontrado.");
@@ -72,40 +73,67 @@ class ProductManager {
     }
 }
 
-    const productManager = new ProductManager();
-    const newProduct = {
-    title: "producto prueba",
-    description: "este es un producto de prueba",
-    price: 200,
-    thumbnail: "sin imagen",
-    code: "abc123",
-    stock: 25,
-};
+const productManager = new ProductManager();
 
-    try {
-        productManager.addProduct(newProduct);
-        console.log("Producto agregado:", newProduct);
-    } catch (error) {
-    console.error(error.message);
-}
-    console.log("Productos después de agregar:", productManager.getProducts());
-    try {
+try {
+    let newProduct; // Declaración fuera del bloque try
+    newProduct = {
+        title: "producto prueba",
+        description: "este es un producto de prueba",
+        price: 200,
+        thumbnail: "sin imagen",
+        code: "abc123",
+        stock: 25,
+    };
+    productManager.addProduct(newProduct);
     const product = productManager.getProductById(newProduct.id);
     console.log("Producto encontrado por ID:", product);
-    } catch (error) {
-        console.error(error.message);
+} catch (error) {
+    console.error(error.message);
+}
+console.log("Productos después de agregar:", productManager.getProducts());
+try {
+    // Elimina la declaración de 'const newProduct' aquí
+    const product = productManager.getProductById(newProduct.id);
+    console.log("Producto encontrado por ID:", product);
+} catch (error) {
+    console.error(error.message);
 }
 
-    try {
-        productManager.updateProduct(newProduct.id, { price: 250, stock: 30 });
-        console.log("Producto actualizado:", productManager.getProductById(newProduct.id));
-        } catch (error) {
-            console.error(error.message);
-        }
 
-    try {
-        productManager.deleteProduct(newProduct.id);
-        console.log("Producto eliminado.");
-    } catch (error) {
-    console.error(error.message);
+    const express = require('express');
+    const app = express();
+    const port = 8080;
+
+
+app.get('/products', (req, res) => {
+    const { limit } = req.query;
+    if (limit) {
+        const limitValue = parseInt(limit);
+        const products = productManager.getProducts().slice(0, limitValue);
+        res.json(products);
+    } else {
+        const products = productManager.getProducts();
+        res.json(products);
     }
+});
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = productManager.getProductById(id);
+        res.json(product);
+    } catch (error) {
+        res.status(404).json({ error: 'Producto no encontrado' });
+    }
+});
+        app.get('/products/limit/:limit', (req, res) => {
+            const limit = parseInt(req.params.limit);
+            const products = productManager.getProducts().slice(0, limit);
+            res.json(products);
+        });
+    app.get('/products/notfound', (req, res) => {
+        res.status(404).json({ error: 'Producto no encontrado' });
+    });
+    app.listen(port, () => {
+        console.log(`Servidor corriendo en http://localhost:${port}`);
+    });
